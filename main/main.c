81436ab76e7440586f2878382b61bc09d8be0a12
tiny_bear <rebeccawilliams9941@gmail.com>
2025-12-29 17:16:24 +1100

    Run hello-world on ESP32-S3.

---
+++ main/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main(void)
+{
+    printf("Hello world!\n");
+
+    while (1) {
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
